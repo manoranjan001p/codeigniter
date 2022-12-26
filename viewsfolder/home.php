1be0cdc28a84f6d77ccf552d12a54ef927f47c92
manoranjan001p <95644446+manoranjan001p@users.noreply.github.com>
2022-12-26 17:26:25 +0530

    Create home.php

---
+++ viewsfolder/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<a href="welcome/registration_page">Registration Page</a>
+<a href="welcome/login">Login here</a>
+    
+</body>
+</html>
