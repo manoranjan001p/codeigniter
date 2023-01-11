6b5448e430a560246af9e2b3f252dd2b302a342e
manoranjan001p <95644446+manoranjan001p@users.noreply.github.com>
2023-01-11 14:02:43 +0530

    Create Product_model.php

---
+++ crud in codeigniter/model/Product_model.php
@@ -0,0 +1,27 @@
+<?php
+ 
+ 
+class Product_model extends CI_Model{
+
+	public function getAllProducts(){
+		$products = $this->db->get('products')->result();
+		return $products;
+	}
+
+	public function storeProduct($data){
+		$this->db->insert('products',$data);
+	}
+
+	public function getProduct($id){
+		return $this->db->where('id',$id)->get('products')->row();
+	}
+
+	public function updateProduct($id,$data){
+		$this->db->where('id',$id)->update('products',$data);
+		//$this->db->
+	}
+
+	public function deleteProduct($id){
+		$this->db->where('id',$id)->delete('products');
+	}
+}
